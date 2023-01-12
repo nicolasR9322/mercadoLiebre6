638b4abc093f6902a1919ab21ccded87674f5a2a
nicolasR9322 <nicolas.a.rodriguez93@gmail.com>
2023-01-12 15:19:50 -0300

    micro desafio 1

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const app = express()
+const path = require("path")
+const PORT = 3000
+app.use(express.static("public"))
+
+app.get("/", (req,res) => {
+    return res.sendFile(path.join(__dirname, "/views/home.html"))
+})
+app.get("home", (req, res) => {
+    return res.sendFile(path.join(__dirname, "/views/home.html"))
+})
+
+
+app.listen(PORT, () => {
+    console.log("Servidor levantado en puerto 3000");
+})
