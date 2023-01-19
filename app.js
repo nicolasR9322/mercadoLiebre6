7e37fc148084af0c1264f33c4f1e349873d09945
nicolasR9322 <nicolas.a.rodriguez93@gmail.com>
2023-01-19 14:40:09 -0300

    desafio 5 formularios

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(express.static("public"))
 app.get("/", (req,res) => {
     return res.sendFile(path.join(__dirname, "/views/home.html"))
 })
-app.get("home", (req, res) => {
+app.get("/home", (req, res) => {
     return res.sendFile(path.join(__dirname, "/views/home.html"))
 })
 
@@ -15,7 +15,15 @@ app.get("/forms", (req, res) => {
     return res.sendFile(path.join(__dirname, "/views/forms.html"))
 })
 
+app.get("/register", (req, res) => {
+    return res.sendFile(path.join(__dirname, "/views/register.html"))
+})
+
+app.get("/login", (req, res) => {
+    return res.sendFile(path.join(__dirname, "/views/login.html"))
+})
+
 
 app.listen(PORT, () => {
-    console.log("Servidor levantado en puerto 3000");
+    console.log(`Servidor levantado en puerto ${PORT} http://localhost:${PORT}`);
 })
